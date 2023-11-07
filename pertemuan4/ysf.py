5533b8e6fb321d869144bd3de54d0db258f2f4b3
xYusufkurnia <148739670+xYusufkurnia@users.noreply.github.com>
2023-11-07 09:47:06 +0700

    Add files via upload

---
+++ pertemuan4/ysf.py
@@ -0,0 +1,9 @@
+# importing sqrt() and factorial from the
+# module math
+import math as mt
+ 
+# if we simply do "import math", then
+# math.sqrt(16) and math.factorial()
+# are required.
+print(mt.sqrt(16))
+print(mt.factorial(6))
